c0942ea741d0d0071984d27cda8127b9aa76c91f
Evan Luo <zl0402@fastmail.com>
2026-02-22 12:34:45 -0800

    feat: Update render graph to include AccumulatePass and adjust edges for proper data flow

--- scripts/ReSTIRBDPT.py
+++ scripts/ReSTIRBDPT.py
@@ -5,10 +5,13 @@ def render_graph_ReSTIRBDPT():
 
     ReSTIRBDPT = createPass("ReSTIRBDPTPass", {})
     g.addPass(ReSTIRBDPT, "ReSTIRBDPTPass")
+    AccumulatePass = createPass("AccumulatePass", {'enabled': True, 'precisionMode': 'Single'})
+    g.addPass(AccumulatePass, "AccumulatePass")
     ToneMapper = createPass("ToneMapper", {'autoExposure': False, 'exposureCompensation': 0.0})
     g.addPass(ToneMapper, "ToneMapper")
 
-    g.addEdge("ReSTIRBDPTPass.color", "ToneMapper.src")
+    g.addEdge("ReSTIRBDPTPass.color", "AccumulatePass.input")
+    g.addEdge("AccumulatePass.output", "ToneMapper.src")
 
     g.markOutput("ToneMapper.dst")
 
